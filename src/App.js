db106b5918a9be288c2bfbf375b6051ac82e574e
Nikhil3522 <nikhiljha3522@gmail.com>
2024-01-18 22:27:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+    const onSubmit = async () => {
+      let data = JSON.stringify({
+          "email": 'a@gmail.com',
+          "password": 'Anurag1'
+      });
+
+      let config = {
+          method: 'POST',
+          maxBodyLength: Infinity,
+          url: 'https://dating-backend-production-f622.up.railway.app/login',
+          withCredentials: true,
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          data: data
+      }
+
+      axios.request(config)
+      .then(async (response) => {
+
+          console.log("response", response);
+          // if(response.data.message === "User LoggedIN!"){
+          //     await localForage.setItem('userLogin', {id: Date.now(), value: true});
+          //     navigate('/home');
+          // }else if(response.data.message === "Wrong Email or Passwod!" ){
+          //     setAlert("Wrong Email or Password");
+          // }else{
+          //     setAlert("Something went wrong! Please try again after sometime.");
+          // }
+      })
+      .catch((error) => {
+          console.log(error);
+      });
+    }
+  return (
+    <div className="App">
+      <button onClick={onSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
